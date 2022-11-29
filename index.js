c66dc377f4c75de9cce81c53c690eb55468033be
Ambaji <ambajiasn@gmail.com>
2022-11-29 06:38:57 +0000

    Solution

---
+++ index.js
@@ -0,0 +1,8 @@
+const element =
+       <div className="bg_contaier">
+<h1>hoee low</h1>
+<img src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"/>
+       </div>
+  //   Write your code here.
+);
+ReactDOM.render(element, document.getElementById("root"));
